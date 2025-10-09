ac8c4ffa081cc15364575d855df83f728050c5f1
Roy Ashba <rashba@psystems.biz>
2025-10-09 23:27:02 +0500

    add heap sort

--- Week_2/heap_sort/sort.py
+++ Week_2/heap_sort/sort.py
@@ -30,12 +30,70 @@
 #     print(a[i])
 
 
-def build_max_heap(arr):
-    pass
+# def heap_sort(arr, i, j):
+#     largest = i
+#     left = i * 2 + 1
+#     right = i * 2 + 2
+#
+#     if left < j and arr[i] < arr[left]:
+#         largest = left
+#
+#     if right < j and arr[largest] < arr[right]:
+#         largest = right
+#
+#     if largest != i:
+#         arr[i], arr[largest] = arr[largest], arr[i]
+#         heapify(arr, n, )
 
-def heapify(arr, n, i):
+
+# def heapify(arr, heap_size, root_index):
+#     largest = root_index
+#     left_child = (2 * root_index) + 1
+#     right_child = (2 * root_index) + 2
+#
+#     if left_child < heap_size and arr[left_child] > arr[largest]:
+#         largest = left_child
+#
+#     if right_child < heap_size and arr[right_child] > arr[largest]:
+#         largest = right_child
+#
+#     if largest != root_index:
+#         arr[largest], arr[root_index] = arr[root_index], arr[largest]
+#         heapify(arr, heap_size, root_index)
+#
+# def heap_sort(arr):
+#     array_length = len(arr)
+#     for i in range(array_length, -1, -1):
+#         heapify(arr, array_length, i)
+#
+#     for i in range(array_length - 1, 0, -1):
+#         arr[i], arr[0] = arr[0], arr[i]
+#         heapify(arr, i, 0)
+#
+# random_list_of_nums = [35, 12, 43, 8, 51, 87, 65]
+# heap_sort(random_list_of_nums)
+# print(random_list_of_nums)
 
 
-array = [22, 11, 88, 66, 55, 33, 77, 44]
-build_max_heap(array)
-print(array)
+def heapify(arr, heap_size, root_index):
+    largest = root_index
+    left_child = (2 * root_index) + 1
+    right_child = (2 * root_index) + 2
+
+    if left_child < heap_size and arr[left_child] > arr[largest]:
+        largest = left_child
+
+    if right_child < heap_size and arr[right_child] > arr[largest]:
+        largest = right_child
+
+    if largest != root_index:
+        arr[largest], arr[root_index] = arr[root_index], arr[largest]
+        heapify(arr, heap_size, root_index)
+
+def heap_sort(arr):
+    for i in range(len(arr) - 1, -1, -1):
+        heapify(arr, len(arr), i)
+
+    for i in range(len(arr) - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)

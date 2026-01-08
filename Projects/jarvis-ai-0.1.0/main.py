457f7e646366c5363da8e22e65e9203243399eae
Roy Ashba <rashba@psystems.biz>
2026-01-09 00:58:11 +0500

    tasks from leetcode

--- Projects/jarvis-ai-0.1.0/main.py
+++
@@ -1,5 +0,0 @@
-from app.handlers.commands import CommandsForJarvis
-
-if __name__ == '__main__':
-    jarvis_command = CommandsForJarvis('Roy')
-    jarvis_command.run()
--- Week_1/Dict/dict.py
+++ Week_1/Dict/dict.py
@@ -79,3 +79,146 @@
 #     print(True)
 # else:
 #     print(False)
+
+# roman = {
+#     'I': 1,
+#     'V': 5,
+#     'X': 10,
+#     'L': 50,
+#     'C': 100,
+#     'D': 500,
+#     'M': 1000
+# }
+# s = str(input('Roman numerical: '))
+# number = 0
+# for letter in s:
+#     for key, value in roman.items():
+#         if letter == key:
+#             number += value
+# print(f'Here is converting roman to integer: {s} = {number}')
+
+# roman = {
+#     'I': 1,
+#     'V': 5,
+#     'X': 10,
+#     'L': 50,
+#     'C': 100,
+#     'D': 500,
+#     'M': 1000
+# }
+#
+# s = input('Converter roman to integer: ')
+# number = 0
+#
+# for i in range(len(s)):
+#     if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+#         number -= roman[s[i]]
+#     else:
+#         number += roman[s[i]]
+# print(f"Result: {number}")
+
+#Wrong
+# def dupl(nums):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] == nums[j]:
+#                 return True
+#     return False
+# print(dupl([1, 2, 3, 1]))
+
+# def dulp(nums):
+#     new_nums = set(nums)
+#     if len(new_nums) < len(nums):
+#         return True
+#     return False
+# print(dulp([1,2,3,1]))
+
+# def dupl(nums):
+#     seen = {}
+#     for i in nums:
+#         if i in seen:
+#             return True
+#         seen[i] = True
+#     return False
+# print(dupl([1,2,3,1]))
+
+
+#Stack
+# n = int(input())
+# def happy_number(n):
+#     first_n = n
+#     list_ = []
+#     while n != 0:
+#         list_.append(n % 10)
+#         n //= 10
+#
+#     for i in list_:
+#         n += i**2
+#         if len(list_) == 1 and 1 in list_:
+#             return True
+#
+#         if len(list_) == 2 and first_n in list_:
+#             return False
+#
+#     return happy_number(n)
+#
+# print(happy_number(7))
+
+# def is_happy(n):
+#     i = 0
+#     j = 0
+#     k = 0
+#     seen = set()
+#     while n != 1 and n not in seen:
+#         seen.add(n)
+#         i = n % 10
+#         n //= 10
+#         j = n % 10
+#         n //= 10
+#         if n:
+#             k = n % 10
+#             n //= 10
+#             result = i**2 + j**2 + k**2
+#             if result == 1:
+#                 return True
+#             else:
+#                 n = result
+#         else:
+#             result = i**2 + j**2
+#             if result == 1:
+#                 return True
+#             else:
+#                 n = result
+#
+#     return False
+#
+# print(is_happy(2))
+
+# def is_happy(n):
+#     seen = set()
+#     while n != 1 and n not in seen:
+#         seen.add(n)
+#         n = sum(int(digit)**2 for digit in str(n))
+#     return n == 1
+#
+# print(is_happy(19))
+
+# def majority_element(nums):
+#     freq = {}
+#     for value in nums:
+#         if value in freq:
+#             freq[value] += 1
+#         else:
+#             freq[value] = 1
+#
+#     majority = 0
+#     k = 0
+#     for key, value in freq.items():
+#         if k < freq[key]:
+#             print(f"key: {key}, value: {freq[key]}")
+#             majority = key
+#             k = freq[key]
+#             print(majority)
+#
+#     return majority
+# print(majority_element([2,2,1,1,1,2,2]))

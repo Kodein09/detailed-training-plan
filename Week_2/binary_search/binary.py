7f2635cb146e16708230b098128c7b63463a3068
Roy Ashba <rashba@psystems.biz>
2026-01-09 01:07:00 +0500

    a little practice

--- Week_2/binary_search/binary.py
+++ Week_2/binary_search/binary.py
@@ -222,20 +222,20 @@
 #
 # print(search_insert_leetcode([1,3,5,6], 7))
 
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if arr[mid] == target:
-            return arr[mid]
-
-        if arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
-print(binary_search([0,1,2,3,4,5,6,7,8,9,10], 8))
+# def binary_search(arr, target):
+#     left = 0
+#     right = len(arr) - 1
+#
+#     while left <= right:
+#         mid = (left + right) // 2
+#
+#         if arr[mid] == target:
+#             return arr[mid]
+#
+#         if arr[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#
+#     return -1
+# print(binary_search([0,1,2,3,4,5,6,7,8,9,10], 8))

c62cd31830ee1f860f3ab59c637802ec35bab4a0
Roy Ashba <rashba@psystems.biz>
2026-01-09 16:59:31 +0500

    merged two dict

--- Week_1/Practical_tasks/merge_two_dict.py
+++ Week_1/Practical_tasks/merge_two_dict.py
@@ -20,3 +20,38 @@
 # merge = MergeDict(d1, d2)
 # print(merge.merge_dict())
 
+# def merge_dict_and_conflict_handling(d1, d2):
+#     return d1 | d2
+#
+# dict_1 = {'a': 1, 'b': 2}
+# dict_2 = {'b': 3, 'c': 4}
+# print(merge_dict_and_conflict_handling(dict_1, dict_2))
+
+# def merge_dict_and_conflict_handling(d1, d2):
+#     result = d1.copy()
+#
+#     for key, value in d2.items():
+#         if key in d1:
+#             result[key] += value
+#         else:
+#             result[key] = value
+#
+#     return result
+#
+# dict_1 = {'a': 1, 'b': 2}
+# dict_2 = {'b': 3, 'c': 4}
+# print(merge_dict_and_conflict_handling(dict_1, dict_2))
+
+def merge_dict_and_conflict_handling(d1, d2):
+    result = d1.copy()
+
+    for key, value in d2.items():
+        if key in d1:
+            result[key] = [d1[key], value]
+        else:
+            result[key] = value
+    return result
+
+dict_1 = {'a': 1, 'b': 2}
+dict_2 = {'b': 3, 'c': 4}
+print(merge_dict_and_conflict_handling(dict_1, dict_2))

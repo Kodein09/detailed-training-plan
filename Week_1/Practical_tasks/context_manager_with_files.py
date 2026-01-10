fe213f72fe806ca0249db319e19e91cc4b36fef1
Roy Ashba <rashba@psystems.biz>
2026-01-10 12:53:34 +0500

    practice with context manager

--- Week_1/Practical_tasks/context_manager_with_files.py
+++ Week_1/Practical_tasks/context_manager_with_files.py
@@ -1,10 +1,33 @@
-def read_file_in_chunks(filename, chunk_size=10):
-    with open(filename, 'r', encoding='utf-8') as file:
-        while True:
-            chunk = file.read(chunk_size)
-            if not chunk:
-                break
-            yield chunk
+# def read_file_in_chunks(filename, chunk_size=10):
+#     with open(filename, 'r', encoding='utf-8') as file:
+#         while True:
+#             chunk = file.read(chunk_size)
+#             if not chunk:
+#                 break
+#             yield chunk
+#
+# f = 'example.txt'
+# print(read_file_in_chunks(f))
 
-f = 'example.txt'
-print(read_file_in_chunks(f))
+# def context_manager(file):
+#     with open(file, 'r') as f:
+#         while f:
+#             read = f.read()
+#             if not read:
+#                 break
+#             else:
+#                 return read
+#     return None
+# print(context_manager('temp.txt'))
+
+class ContextManager:
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        read = open(self.file, 'r')
+        self.file = read
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()

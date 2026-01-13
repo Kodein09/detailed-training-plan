0ed6c92940a61eebdd5e1e1c4a44008dbf0ce6e7
Roy Ashba <rashba@psystems.biz>
2026-01-13 22:31:08 +0500

    practice with another methods fot combinations

--- Week_1/Itertools/combinations.py
+++ Week_1/Itertools/combinations.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement, product
 
 # example = 'abcd'
 # comb = combinations(example, 2)
@@ -10,11 +10,27 @@ from itertools import combinations
 # print(next(comb))
 
 
-friends = ['Roy', 'Arif', 'Klim', 'Anton']
-comb = combinations(friends, 2)
-print(next(comb))
-print(next(comb))
-print(next(comb))
-print(next(comb))
-print(next(comb))
-print(next(comb))
+# friends = ['Roy', 'Arif', 'Klim', 'Anton']
+# comb = combinations(friends, 2)
+# print(next(comb))
+# print(next(comb))
+# print(next(comb))
+# print(next(comb))
+# print(next(comb))
+# print(next(comb))
+
+#combinations
+# data = combinations(range(3), 2)
+# for i in data:
+#     print(i)
+
+#combination_with_replacement
+# cwr = combinations_with_replacement(range(3), 2)
+# for i in cwr:
+#     print(i)
+
+#product
+# gen = product(range(2), repeat=3)
+# for x in gen:
+#     print(x)
+

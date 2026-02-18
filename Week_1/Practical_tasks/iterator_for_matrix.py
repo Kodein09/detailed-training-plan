fc6ef6d01839efb90a7bc8bedfb7d4c7f3f41254
Roy Ashba <rashba@psystems.biz>
2026-02-18 22:39:19 +0500

    create spiral iterator for matrix

--- Week_1/Practical_tasks/iterator_for_matrix.py
+++ Week_1/Practical_tasks/iterator_for_matrix.py
@@ -135,3 +135,74 @@
 # spiral = SpiralMatrix(matrix)
 # for value in spiral:
 #     print(value, end=' ')
+
+
+# class MatrixIterator:
+#     def __init__(self, matrix):
+#         self.matrix = matrix
+#
+#     def __iter__(self):
+#         return iter(self.matrix)
+#
+#     def iterator_for_matrix(self):
+#         top = 0
+#         bottom = len(self.matrix)
+#         left = 0
+#         right = len(self.matrix[0])-1
+#
+#         for i in range(left, right+1):
+#             print(self.matrix[top][i])
+#
+#         for j in range(1, bottom):
+#             print(self.matrix[j][right])
+#
+#         for k in range(right-1, -1, -1):
+#             print(self.matrix[bottom-1][k])
+#
+#         for l in range(left, right):
+#             print(self.matrix[left+1][l])
+#
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# matrix_iter = MatrixIterator(matrix)
+# print(matrix_iter.iterator_for_matrix())
+
+
+class MatrixIterator:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def iterator_for_matrix(self):
+        left, right = 0, len(self.matrix) - 1
+        top, bottom = 0, len(self.matrix) - 1
+
+        while top <= bottom and left <= right:
+            for i in range(left, right+1):
+                yield self.matrix[left][i]
+            top += 1
+
+            for j in range(top, bottom+1):
+                yield self.matrix[j][right]
+            right -= 1
+
+            if top <= bottom:
+                for k in range(right, left-1, -1):
+                    yield self.matrix[bottom][k]
+                bottom -= 1
+
+            if left <= right:
+                for l in range(bottom, top-1, -1):
+                    yield self.matrix[l][left]
+                left += 1
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+m = MatrixIterator(matrix)
+for num in m.iterator_for_matrix():
+    print(num)

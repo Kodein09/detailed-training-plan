80f9f18ab21e59fd06e0e9d279c44d3b82820053
Roy Ashba <rashba@psystems.biz>
2026-02-20 01:05:45 +0500

    word guessing game update

--- Projects/Word_Guessing_Game/main.py
+++ Projects/Word_Guessing_Game/main.py
@@ -1,9 +1,28 @@
 import random
 
 class WordGuessingGame:
-    def play(self, words):
+    def play(self, w):
         word = random.choice(words)
         total_guesses = 0
+        failed = 0
         turns = 12
-        
+        seen = []
 
+        user = input("What is your name?\n")
+        print(f"Good luck, {user}!")
+        while turns > 0:
+            turns -= 1
+            guessing = input("Guess a character: ")
+            total_guesses += 1
+            for char in word:
+                if char == guessing:
+                    print(char, end="\n")
+                else:
+                    print("_")
+
+
+        print(f"You run out of turns: {turns}")
+
+words = ['decomposition', 'computer', 'redemption', 'river', 'rock', 'reverse', 'water', 'fan', 'science']
+gg = WordGuessingGame()
+gg.play(words)

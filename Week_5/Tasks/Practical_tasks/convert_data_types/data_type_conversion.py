adb3c0bbce8f48d6786f2951365ecb4627208e0b
Roy Ashba <rashba@psystems.biz>
2026-04-12 15:26:21 +0500

    converted data types

--- Week_5/Tasks/Practical_tasks/convert_data_types/data_type_conversion.py
+++ Week_5/Tasks/Practical_tasks/convert_data_types/data_type_conversion.py
@@ -1,20 +1,22 @@
 import pandas as pd
+import numpy as np
 
-#df = pd.read_csv("")
-# print(df)
-#
+
+#---------- astype
+df = pd.DataFrame(({"Name": ["Roy"], "Age": [20], "Height": [183]}))
+astype = df.astype({"Age": "float", "Height": "bool"})
+print("df dtypes:", df.dtypes)
+print("", pd.DataFrame(data=astype).dtypes)
+
+
+
+#---------- to_numeric
 # df["Genre_num"] = df["Genre"].map({"Male": 0, "Female": 1})
 # to_num = pd.to_numeric(df["Genre_num"], errors="coerce")
 # print(to_num)
 
 
-#to_date
-df = pd.DataFrame({"Birth": ["1990-05-21", "2001-04-06", "2004/09/29"]})
-df["Birth_date"] = pd.to_datetime(df["Birth"], format="mixed", errors="coerce")
-print(df)
-
-#astype
-df = pd.DataFrame(({"Name": ["Roy"], "Age": [20], "Height": [183]}))
-astype = df.astype({"Age": "float", "Height": "bool"})
-#astype = df.astype(str)
-print(astype.dtypes)
+#---------- to_datetime
+# df = pd.DataFrame({"Birth": ["1990-05-21", "2001-04-06", "2004/09/29"]})
+# df["Birth_date"] = pd.to_datetime(df["Birth"], format="mixed", errors="coerce")
+# print(df)

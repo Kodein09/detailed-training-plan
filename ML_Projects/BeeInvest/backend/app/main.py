4e964bec26cfda7d1fd87bfce30874b9f3b9377b
Roy Ashba <rashba@psystems.biz>
2026-05-13 16:49:01 +0500

    feat: app fastapi, cors

--- ML_Projects/BeeInvest/backend/app/main.py
+++ ML_Projects/BeeInvest/backend/app/main.py
@@ -1,1 +1,16 @@
-import fastapi
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="🐝Invest",
+    description="Investment Website"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

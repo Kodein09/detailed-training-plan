64a28b464cb48f8bd71f1329e71a5496ad139657
Roy Ashba <rashba@psystems.biz>
2026-05-27 23:15:56 +0500

    feat: eda distribution and outliers

--- Week_9/Practical/exploratory_data_analysis/eda.py
+++ Week_9/Practical/exploratory_data_analysis/eda.py
@@ -10,24 +10,60 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from IPython.core.pylabtools import figsize
 
 df = pd.read_csv("../train.csv")
 
-numeric_df = df.select_dtypes(include=np.number)
+#CORRELATION MATRIX
+# numeric_df = df.select_dtypes(include=np.number)
+#
+# corr_matrix = numeric_df.corr()
+#
+# sns.heatmap(
+#     corr_matrix,
+#     cmap='coolwarm',
+#     annot=True,
+#     fmt='.2f',
+#     linewidths=1,
+#     linecolor='black',
+#     robust=True,
+#     square=True
+# )
+#
+# print(df.info())
+# plt.title("Correlation Matrix")
+# plt.show()
 
-corr_matrix = numeric_df.corr()
 
-sns.heatmap(
-    corr_matrix,
-    cmap='coolwarm',
-    annot=True,
-    fmt='.2f',
-    linewidths=1,
-    linecolor='black',
-    robust=True,
-    square=True
-)
 
-print(df.info())
-plt.title("Correlation Matrix")
+#DISTRIBUTON
+# fig, axes = plt.subplots(1,2, figsize=(15, 5))
+# print(fig)
+# print(axes)
+#
+# sns.histplot(df['Age'].dropna(), kde=True, ax=axes[0], color='skyblue')
+# axes[0].set_title("Age Distribution")
+# axes[0].set_xlabel('Age')
+#
+# sns.histplot(df['Fare'], kde=True, ax=axes[1], color='RoyalBlue')
+# axes[1].set_title("Ticket Cost Distribution")
+# axes[1].set_xlabel('Fare')
+#
+# plt.tight_layout()
+# plt.show()
+
+
+
+#Outliers
+fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+sns.boxplot(df['Age'], ax=axes[0], color='RoyalBlue')
+axes[0].set_title("Age Outliers")
+axes[0].set_xlabel('Age')
+
+sns.boxplot(df['Fare'], ax=axes[1], color='steelblue')
+axes[1].set_title("Fare Outliers")
+axes[1].set_xlabel('Fare')
+
+plt.tight_layout()
 plt.show()
